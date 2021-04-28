8d0f4c7bc9d57c1573ca32bc13192c7d90308fbf
pamelakelly0023 <pamela.kelly95@gmail.com>
2021-04-28 16:27:22 -0300

    exemplo de codigo com fluxo sincrono

---
+++ text_sync.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+for (let i = 1; i <= 5; i++) {
+	const file = `sync-txt${i}.txt`;
+	const out = fs.writeFileSync(file, 'Hello Node.js!');
+	console.log(out);
+}
+
+// Timeline síncrona bloqueante = 1.000 milissegundos
